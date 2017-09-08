7e9f55d7ebbabecc64b170044e8d458c6a164158
Yingying Chen <ychen1030@qq.com>
2017-09-08 07:35:10 +0000

    finish part 1

--- app.rb
+++ app.rb
@@ -40,6 +40,14 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
+    if @game.guesses.include? letter or @game.wrong_guesses.include?letter
+      flash[:message] = "You have already used that letter."
+    elsif !letter.match(/[[:alpha:]]/)
+      flash[:message] = "Invalid guess."
+    else
+      @game.guess(letter)
+    end
+    
     redirect '/show'
   end
   
@@ -50,17 +58,31 @@ class HangpersonApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
-    erb :show # You may change/remove this line
+    if @game.check_win_or_lose == :win
+      redirect '/win'
+    elsif @game.check_win_or_lose == :lose
+      redirect '/lose'
+    else
+      erb :show 
+    end # You may change/remove this line
   end
   
   get '/win' do
     ### YOUR CODE HERE ###
-    erb :win # You may change/remove this line
+    if @game.check_win_or_lose == :win
+      erb :win
+    else
+      redirect '/show'
+    end
   end
   
   get '/lose' do
     ### YOUR CODE HERE ###
-    erb :lose # You may change/remove this line
+    if @game.check_win_or_lose == :lose
+      erb :lose # You may change/remove this line
+    else
+      redirect '/show'
+    end
   end
   
 end
--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -8,8 +8,12 @@ class HangpersonGame
   # def initialize()
   # end
   
+  attr_accessor :guesses, :wrong_guesses, :word
+  
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
@@ -23,5 +27,44 @@ class HangpersonGame
       return http.post(uri, "").body
     }
   end
-
+  
+  def guess(letter)
+    if not letter =~ /[[:alpha:]]/
+      raise ArgumentError
+    else 
+      letter.downcase!
+      if @word.include? letter and !@guesses.include? letter
+        @guesses += letter
+        return true
+      elsif !@word.include? letter and !@wrong_guesses.include? letter
+        @wrong_guesses += letter
+        return true
+      else
+        return false
+      end
+    end
+  end
+  
+  def word_with_guesses
+    result = ""
+    for i in @word.each_char
+      if @guesses.include? i
+        result += i
+      else
+        result += "-"
+      end
+    end
+    return result
+  end
+  
+  def check_win_or_lose
+    if @word == word_with_guesses
+      return :win
+    elsif @wrong_guesses.length == 7
+      return :lose
+    else
+      return :play
+    end
+  end
+    
 end
